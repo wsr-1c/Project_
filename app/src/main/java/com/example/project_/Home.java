2cc1284797e3999850f9d5774c34385de24a378c
WSR-1 <iwsr1@mail.ru>
2025-03-11 16:47:49 +0800

    2.0

--- app/src/main/java/com/example/project_/Home.java
+++ app/src/main/java/com/example/project_/Home.java
@@ -24,7 +24,7 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_on_board1);
+        setContentView(R.layout.activity_homes);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

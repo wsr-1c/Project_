6cc87e730cbc7769d9ad655e8b9fa64ffebed029
WSR-1 <iwsr1@mail.ru>
2025-03-12 15:37:30 +0800

    4.0

--- app/src/main/java/com/example/project_/Forgot.java
+++ app/src/main/java/com/example/project_/Forgot.java
@@ -1,6 +1,11 @@
 package com.example.project_;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Forgot extends AppCompatActivity {
+    ImageButton back;
+    Button send;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +29,27 @@ public class Forgot extends AppCompatActivity {
             return insets;
         });
 
+        send = findViewById(R.id.button4);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validEmail()){
+                    Intent intent = new Intent(Forgot.this, S1_SignIn.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
 
+        back = findViewById(R.id.img10000);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Forgot.this, S1_SignIn.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
